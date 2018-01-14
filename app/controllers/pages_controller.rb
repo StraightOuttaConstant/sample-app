19825592064c8555a8f1b1e743b3026260e8e877
JackMarx <seriousfools@gmail.com>
2018-01-14 09:58:53 -0600

    updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def hello_method
-    render json: {message: "hello"}
+    sum = 1 + 1
+    time = Time.now.strftime("%I:%M %p")
+    puts "this goes to the terminal"
+    render json: {message: "hello", sum: sum, current_time: time}
   end
 
   def goodbye_method
